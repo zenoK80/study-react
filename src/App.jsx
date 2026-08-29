4c099d4aaba935294e0c6152c7e874100ed62894
zenoK80 <stcfs80@gmail.com>
2026-08-29 22:30:15 +0900

    feat: part1-07 map 및 filter 기반 리스트 렌더링 구현 (study)

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import ReactRenderingProcess from './part1/02_react_rendering_process';
 import Hello from './part1/03_Hello';
 import Greeting from './part1/04_Greeting';
 import ProfileHeader from './part1/05_ProfileHeader';
+import Profile from './part1/06_ChildrenPractice';
+import ListRendering from './part1/07_ListRendering';
 
 // Part2 미션
 import Cafeteria from './part2/01_Cafeteria';
 import ShoppingCart from './part2/02_ShoppingCart';
 import TicketBooking from './part2/03_TicketBooking';
 
+
+
 function App() {
   return (
     <div>
-      <ProfileHeader name="Yongsu" age={20} country="Korea" />
+      <ListRendering></ListRendering>
     </div>
   );
 }
